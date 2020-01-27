8884ad329273e759f8252d25e4b944739b1f31ef
juliocesarmfo <60270538+juliocesarmfo@users.noreply.github.com>
2020-01-27 13:38:44 -0300

    Changing Website Behavior/CSS + New API Fetch
    Não havia percebido que a intenção da consulta API era retornar também as próximas páginas (3, 4 , 5...), portanto reduzi a função do fetch da API e descartei a animação de show less products. Também foi implementado um timeout para o botão, evitando spam clicking. Pequenas alterações no CSS. Agora o website roda versões minificadas do JS e CSS, porém as versões originais ainda estão em seus respectivos diretórios.

--- js/global_scripts.js
+++ js/global_scripts.js
@@ -1,32 +1,29 @@
+let trigger = false;
+let url = `https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1`;
+
 /* Fetching the API */
 async function getURL() {
-	fetch(`https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1`)
+	fetch(url)
 	.then(response => response.json())
 	.then(function(data) {
-		showItems(data);
+		if (!trigger) {
+			showItems(data);
+		}
+		else {
+			url = `https://` + data.nextPage;
+			trigger = false;
+			getURL();
+		}
 	});
 }
+
 /* Function to see more products */
-function openMore() {
-	fetch(`https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=2`)
-	.then(response => response.json())
-	.then(function(data) {
-		showItems(data);
-	});
-	
-	document.getElementById(`show-more-button`).id = `show-less-button`;
-	document.getElementById(`show-less-button`).innerHTML = `Mostrar menos`;
-	document.getElementById(`show-less-button`).onclick = showLess;
-}
-/* Function to show less products */
-function showLess() {
-	for (let i = 15; i > 7; i--) {
-		document.getElementsByClassName(`product-container`)[i].style.animation = `fade-out-anim 0.3s linear`;
-		setTimeout(function(){document.getElementsByClassName(`product-container`)[i].remove();}, 300);
-	}
-	document.getElementById(`show-less-button`).id = `show-more-button`;
-	document.getElementById(`show-more-button`).innerHTML = `Ainda mais produtos aqui!`;
-	document.getElementById(`show-more-button`).onclick = openMore;
+function openMore(button) {
+	button.disabled = true;
+	trigger = true;
+	getURL();
+	// Comportamento anti-spam-clicking
+	setTimeout(function() { button.disabled = false; }, 1000);
 }
 
 onload = getURL;
